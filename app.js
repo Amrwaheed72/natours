e85c58686babdde0fa240d3b3362cca013560f7c
Amr Waheed <amrmohamed877@gmail.com>
2025-03-23 02:23:36 +0200

    finishing part of get & post requests

--- app.js
+++ app.js
@@ -1,15 +1,55 @@
 import express from 'express';
-
+import fs from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
-
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'hello from the server ',app:"features" })
+
+app.use(express.json())
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
+    res.status(404).json({
+        status: 'fail',
+        message: 'error in the client'
+    })
+    res.status(500).json({
+        status: 'error',
+        message: 'server error'
+    })
 })
-app.post('/',(req,res)=>{
-    res.send('u can post to this input')
+
+app.post('/api/v1/tours', (req, res) => {
+    const id = tours[tours.length - 1].id + 1
+    const newTour = Object.assign({ id: id }, req.body)
+
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
+    })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+
 })
 
 
