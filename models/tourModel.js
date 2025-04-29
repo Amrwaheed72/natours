2c342df2cc03b525805a81605df706d7f863e452
Amr Waheed <amrmohamed877@gmail.com>
2025-04-29 05:28:46 +0300

    finishing the model

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,17 +6,58 @@ const tourSchema = new mongoose.Schema({
         type: String,
         required: [true, 'A tour must have a name'],
         unique: true,
-    }, rating: {
+        trim: true,
+    },
+    duration: {
+        type: Number,
+        required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a group size']
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must a difficulty']
+    },
+    ratingsAverage: {
         type: Number,
         default: 4.5,
         min: [1, 'Rating must be above 1.0'],
         max: [5, 'Rating must be below 5.0'],
-    }, price: {
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0
+    },
+    price: {
         type: Number,
         required: [true, 'A tour must have a price'],
-    }
+    },
+    priceDiscount: {
+        type: Number
+    },
+    summary: {
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have a summary']
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have a cover image']
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    startDates: [Date]
 })
 
 const Tour = mongoose.model('Tour', tourSchema)
 
-export  {Tour}
+export { Tour }
