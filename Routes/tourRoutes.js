eea3161128b49b3d0fb38e9a5b08b4ddc61c5243
Amr Waheed <amrmohamed877@gmail.com>
2025-06-18 20:08:26 +0300

    finishing the aggregation pipeline

--- Routes/tourRoutes.js
+++ Routes/tourRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllTours, createTour, getTour, updateTour, deleteTour, aliasTopTours } from '../controllers/tourController.js'
+import { getAllTours, createTour, getTour, updateTour, deleteTour, aliasTopTours, getTourStats, getMonthlyPlan } from '../controllers/tourController.js'
 
 
 const router = express.Router();
@@ -7,7 +7,8 @@ const router = express.Router();
 // router.param('id', checkID)
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours) // Middleware to get top 5 cheap tours
-
+router.route('/tour-stats').get(getTourStats)
+router.route('/monthly-plan/:year').get(getMonthlyPlan)
 router.route('/').get(getAllTours).post(createTour)
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour)
 
